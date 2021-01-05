9c9acb98e5cd22133b662c0ff994f5526bd90bbf
Cy-Qy <308103222@qq.com>
2021-01-05 14:08:43 +0800

    增加404

--- server.js
+++ server.js
@@ -42,6 +42,10 @@ var server = http.createServer(function(request, response){
     response.end()
   } else {
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`
+      <h1>你访问的页面不存在</h1>
+    `)
     response.end()
   }
 
